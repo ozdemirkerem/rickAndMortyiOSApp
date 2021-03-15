f19e3a02a2c43cf3a7e574d25ecc6c6092c1d3fd
keremozdemir28 <ozdemirk@mef.edu.tr>
2021-03-15 19:29:12 +0300

    Initial Commit

---
+++ rickandmortyiOSApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  rickandmortyiOSApp
+//
+//  Created by Kerem Özdemir on 15.03.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ rickandmortyiOSApp/rickandmortyiOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  rickandmortyiOSAppApp.swift
+//  rickandmortyiOSApp
+//
+//  Created by Kerem Özdemir on 15.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct rickandmortyiOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
